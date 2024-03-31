30a806f249d6d16ba169223ae21eb69e0eec0caf
Abhishek Kumar <72148582+ABHIROMAN@users.noreply.github.com>
2024-03-31 20:44:51 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      By{" "}
+      <a
+        href="https://github.com/ABHIROMAN"
+        target="_blank"
+        alt="Abhishek Kumar's GitHub Account"
+        className="hover:underline"
+      >
+        Abhishek Kumar
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
